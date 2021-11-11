552b2ab69d9012e0456a2a06fecb721c6188d562
Asad <Asadullah-Chaudry@hotmail.com>
2021-11-11 21:53:26 +0100

    Vorige versie was niet juist
    contast aangemaakt

--- Snake/main.cpp
+++ Snake/main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
-
 using namespace std;
+bool gameOver;
 
+const int width = 20;
+const int height = 20;
+int x,y, fruitX, FruitY, score;
+
+
+
+
+void Setup()
+{
+    gameOver = false;
+}
+void Draw()
+{
+
+}
+void Input()
+{
+
+}
+void Logic()
+{
+
+}
 int main()
 {
-    cout << "Hello World!" << endl;
+    Setup();
+    while (!gameOver)
+    {
+        Draw();
+        Input();
+        Logic();
+    }
     return 0;
 }
