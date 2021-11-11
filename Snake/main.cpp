cbf4c54d74d6f05b8dc01c13a0ea590cf3133adf
Asad <Asadullah-Chaudry@hotmail.com>
2021-11-12 00:42:53 +0100

    Muren gemaakt
    Muren gemaakt voor snake spel

--- Snake/main.cpp
+++ Snake/main.cpp
@@ -4,7 +4,10 @@ bool gameOver;
 
 const int width = 20;
 const int height = 20;
-int x,y, fruitX, FruitY, score;
+int x,y, fruitX, fruitY, score; //Hoofd en fruit positie
+
+enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN}; // om positie te zien
+eDirection dir;
 
 
 
@@ -12,10 +15,39 @@ int x,y, fruitX, FruitY, score;
 void Setup()
 {
     gameOver = false;
-}
-void Draw()
-{
+    dir = STOP;
+    x = width / 2; //zodat snake kop gecenterd wordt
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
 
+}
+void DrawWall()
+{
+    system("cls"); //system("clear");
+    for (int i = 0; i < width +2; i++)// top van het bord
+        cout << "#";
+      cout << endl; //next line
+
+      for(int i = 0; i < width; i++)
+      {
+          for(int j = 0; j < width; j++)
+          {
+            if (j==0)
+               cout << "#"; // als j = 0 dan print hij # anders een spactie
+               cout << " ";
+
+            if (j == width - 1)
+                cout << "#";
+
+          }
+          cout << endl;
+      }
+
+    for (int i = 0; i < width +2; i++)
+        cout << "#";
+      cout << endl; //next line
 }
 void Input()
 {
@@ -30,7 +62,7 @@ int main()
     Setup();
     while (!gameOver)
     {
-        Draw();
+        DrawWall();
         Input();
         Logic();
     }
